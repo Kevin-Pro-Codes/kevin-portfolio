fa2acfaa1a5feebab66526421c92f46eace5ed7b
Kevin <kevinprofessionalman@outlook.com>
2025-06-09 17:37:41 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './index.css';
+import './App.css';
+import './styles/Home.css';
+import './styles/Gallery.css';
+import './styles/Projects.css';
+import './styles/Contact.css';
 import App from './App.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
