8c87b0978a97c3ea031c55b00f7ab61d1e4fc034
Kevin <admin@looping>
2026-01-08 15:59:31 -0300

    .

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import pt from './locales/pt.json';
-import en from './locales/en.json';
+import en from './i18n/locales/en.json';
+import pt from './i18n/locales/pt.json';
 
 i18n
   .use(LanguageDetector)
